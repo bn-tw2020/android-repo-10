b4737fe698c52403b2bee9839b419651ddce991c
taewoo <66770613+bn-tw2020@users.noreply.github.com>
2022-07-12 17:08:52 +0900

    feat: Koin 의존성 주입 적용 (#20)
    - 앱의 의존성 주입을 위해 Koin 적용
    - context 설정
    - Logging 설정

---
+++ app/src/main/java/com/github/repo/application/GithubRepositoryApplication.kt
@@ -0,0 +1,19 @@
+package com.github.repo.application
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class GithubRepositoryApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@GithubRepositoryApplication)
+            modules()
+        }
+    }
+}
