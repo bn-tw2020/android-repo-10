92327e5e8af4961618a3b3ebc344dfdac6beebeb
Zzu-h <juho7785@naver.com>
2022-07-13 12:43:22 +0900

    style: 안쓰는 import 제거
    - 안쓰는 import android.util.Log 문 제거

--- app/src/main/java/com/github/repo/presentation/main/MainActivity.kt
+++ app/src/main/java/com/github/repo/presentation/main/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.github.repo.presentation.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
