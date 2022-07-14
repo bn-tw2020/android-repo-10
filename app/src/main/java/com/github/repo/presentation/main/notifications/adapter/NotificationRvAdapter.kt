508c5f2de70aaa8a67efca7f8b223d853e79e68d
Zzu-h <juho7785@naver.com>
2022-07-14 11:50:04 +0900

    feat: RecyclerView item 이미지 로드 및 업데이트 시간 수정
    - Glide를 이용한 이미지 로드
    - 현재 Date와 불러온 Date를 비교해 언제 업데이트 되었는지를 판단하는 함수 추가

--- app/src/main/java/com/github/repo/presentation/main/notifications/adapter/NotificationRvAdapter.kt
+++ app/src/main/java/com/github/repo/presentation/main/notifications/adapter/NotificationRvAdapter.kt
@@ -1,11 +1,13 @@
 package com.github.repo.presentation.main.notifications.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.github.repo.databinding.ItemNotificationBinding
 import com.github.repo.domain.dto.NotificationDto
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NotificationRvAdapter() : RecyclerView.Adapter<NotificationRvAdapter.ViewHolder>() {
 
@@ -32,10 +34,33 @@ class NotificationRvAdapter() : RecyclerView.Adapter<NotificationRvAdapter.ViewH
 
     override fun getItemCount(): Int = notificationDtoList.size
 
+    private val mFormat: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss")
+    private val currentDate: Date by lazy { Date(System.currentTimeMillis()) }
+
+    private fun getUpdateDate(date: String): String {
+        val updateDate: Date = mFormat.parse(date)
+        updateDate.compareTo(currentDate)
+
+        val diff: Long = currentDate.getTime() - updateDate.getTime()
+        val seconds = diff / 1000
+        val minutes = seconds / 60
+        val hours = minutes / 60
+        val days = hours / 24
+        val years = days / 365
+
+        return if (years > 0) "${years}년 전"
+        else if (days > 0) "${days}일 전"
+        else if (hours > 0) "${hours}시간 전"
+        else if (minutes > 0) "${minutes}분 전"
+        else "${seconds}초 전"
+    }
+
     inner class ViewHolder(val binding: ItemNotificationBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(notificationDto: NotificationDto) {
-            binding.ivThumbnail
+            Glide.with(binding.root)
+                .load(notificationDto.thumbnailUrl)
+                .into(binding.ivThumbnail)
 
             binding.tvRepoName.text = notificationDto.repoName
             binding.tvNotificationTitle.text = notificationDto.notificationTitle
@@ -43,10 +68,6 @@ class NotificationRvAdapter() : RecyclerView.Adapter<NotificationRvAdapter.ViewH
 
             binding.tvUpdateTime.text = getUpdateDate(notificationDto.updateTime)
             binding.tvCommentCount.text = notificationDto.commentCount.toString()
-        }
-
-        private fun getUpdateDate(date: String): String {
-            return ""
         }
     }
 }
