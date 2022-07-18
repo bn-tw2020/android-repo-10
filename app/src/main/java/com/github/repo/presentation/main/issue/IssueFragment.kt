803e7f9b901bd3d92f2ddf64fabb97b2c2566f74
bn-tw2020 <skaxodn97@gmail.com>
2022-07-18 15:22:38 +0900

    ui: issue 레이아웃
    - 스피너 레이아웃
      - 커스텀 어댑터
    - 리사이클러 뷰 레이아웃
      - 뷰의 목록 아이템 레이아웃

--- app/src/main/java/com/github/repo/presentation/main/issue/IssueFragment.kt
+++ app/src/main/java/com/github/repo/presentation/main/issue/IssueFragment.kt
@@ -4,21 +4,58 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import com.github.repo.R
+import com.github.repo.databinding.FragmentIssueBinding
+import com.github.repo.presentation.main.issue.adapter.SpinnerAdapter
 
 class IssueFragment : Fragment() {
+
+    private lateinit var binding: FragmentIssueBinding
+    private val spinnerAdapter by lazy { SpinnerAdapter(requireContext()) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_issue, container, false)
+    ): View {
+        binding = FragmentIssueBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initView()
+    }
+
+    private fun initView() {
+        setSpinnerSetting()
+    }
+
+    private fun setSpinnerSetting() {
+        binding.spinnerIssueFilter.adapter = spinnerAdapter
+        binding.layoutFilter.setOnClickListener { binding.spinnerIssueFilter.performClick() }
+        binding.spinnerIssueFilter.viewTreeObserver.addOnWindowFocusChangeListener {
+            when (it) {
+                true -> binding.layoutFilter.setBackgroundResource(R.drawable.btn_deactive_14dp)
+                else -> binding.layoutFilter.setBackgroundResource(R.drawable.btn_deactive_focus_14dp)
+            }
+        }
+
+        binding.spinnerIssueFilter.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    spinnerAdapter.setSelectedPosition(position)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) = Unit
+            }
     }
 }
---
+++ app/src/main/java/com/github/repo/presentation/main/issue/adapter/SpinnerAdapter.kt
@@ -0,0 +1,58 @@
+package com.github.repo.presentation.main.issue.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.CheckedTextView
+import androidx.core.content.ContextCompat
+import com.github.repo.R
+import com.github.repo.databinding.ItemSpinnerContentBinding
+import com.github.repo.databinding.ItemSpinnerTitleBinding
+
+class SpinnerAdapter(private val context: Context) : BaseAdapter() {
+
+    private val items: List<String> = listOf("Open", "Closed", "All")
+    private var selectedPosition = 0
+
+    override fun getCount(): Int = items.size
+
+    override fun getItem(position: Int): Any = items[position]
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = convertView
+            ?: LayoutInflater.from(parent?.context)
+                .inflate(R.layout.item_spinner_title, parent, false)
+        val binding = ItemSpinnerTitleBinding.bind(view)
+        binding.tvSpinnerTitle.text = items[position]
+        return binding.root
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val binding =
+            ItemSpinnerContentBinding.inflate(LayoutInflater.from(parent?.context), parent, false)
+        binding.tvSpinnerContent.text = items[position]
+        onChecked(position, binding.tvSpinnerContent)
+        return binding.root
+    }
+
+    private fun onChecked(position: Int, view: CheckedTextView) {
+        when (position) {
+            selectedPosition -> {
+                view.isChecked = true
+                view.setTextColor(ContextCompat.getColor(context, R.color.white))
+            }
+            else -> {
+                view.isChecked = false
+                view.setTextColor(ContextCompat.getColor(context, R.color.grey))
+            }
+        }
+    }
+
+    fun setSelectedPosition(position: Int) {
+        selectedPosition = position
+    }
+}
