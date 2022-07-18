944e5c8c3bfffd05a55954449d31138117b4a4ee
Zzu-h <juho7785@naver.com>
2022-07-18 23:11:52 +0900

    feat: viewmodle 의존성 주입
    - ProfileViewModel 의존성 주입

--- app/src/main/java/com/github/repo/di/ViewModelModule.kt
+++ app/src/main/java/com/github/repo/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package com.github.repo.di
 import com.github.repo.presentation.login.LoginViewModel
 import com.github.repo.presentation.search.SearchViewModel
 import com.github.repo.presentation.main.notifications.NotificationsViewModel
+import com.github.repo.presentation.profile.ProfileViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -10,4 +11,5 @@ val viewModelModule = module {
     viewModel { LoginViewModel(get(), get()) }
     viewModel { SearchViewModel(get()) }
     viewModel { NotificationsViewModel(get(), get()) }
+    viewModel { ProfileViewModel(get(), get()) }
 }
